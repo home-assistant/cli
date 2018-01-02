192d9e07b944c376709df68eaf24e24dbd799240
Ryan McLean <pvtryan100@gmail.com>
2018-01-02 21:11:35 +0000

    Fixed Formatting

--- command/homeassistant.go
+++ command/homeassistant.go
@@ -1,55 +1,55 @@
 package command
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/home-assistant/hassio-cli/command/helpers"
-	"github.com/urfave/cli"
-	"os"
-	"strings"
+    "github.com/home-assistant/hassio-cli/command/helpers"
+    "github.com/urfave/cli"
+    "os"
+    "strings"
 )
 
 func CmdHomeassistant(c *cli.Context) {
-	const HASSIO_BASE_PATH = "homeassistant"
-	action := ""
-	endpoint := ""
-  serverOverride := ""
-	get := false
-	if c.NArg() > 0 {
-		action = c.Args()[0]
-	}
+    const HASSIO_BASE_PATH = "homeassistant"
+    action := ""
+    endpoint := ""
+    serverOverride := ""
+    get := false
+    if c.NArg() > 0 {
+        action = c.Args()[0]
+    }
 
-	switch action {
-	case "info",      // GET
-		 "logs":
-		endpoint = action
-		get = true
-	case "check",     // POST
-	     "restart",
-	     "start",
-	     "stop",
-	     "update":
-		endpoint = action
-	default:
-		fmt.Fprintf(os.Stderr, "No valid action detected")
-		os.Exit(3)
-	}
+    switch action {
+    case "info",      // GET
+        "logs":
+        endpoint = action
+        get = true
+    case "check",     // POST
+        "restart",
+        "start",
+        "stop",
+        "update":
+        endpoint = action
+    default:
+        fmt.Fprintf(os.Stderr, "No valid action detected")
+        os.Exit(3)
+    }
 
-	if endpoint != "" {
-    uri := helpers.GenerateUri(HASSIO_BASE_PATH, endpoint, serverOverride)
-    response := helpers.RestCall(uri, get,  c.String("options"))
+    if endpoint != "" {
+        uri := helpers.GenerateUri(HASSIO_BASE_PATH, endpoint, serverOverride)
+        response := helpers.RestCall(uri, get,  c.String("options"))
 
-		if c.String("filter") == "" {
-			helpers.DisplayOutput(response, c.Bool("rawjson"))
-		} else {
-			filter := strings.Split(c.String("filter"), ",")
-			data := helpers.FilterProperties(response, filter)
-			helpers.DisplayOutput(data, c.Bool("rawjson"))
-		}
-		responseMap := helpers.ByteArrayToMap(response)
-		result := responseMap["result"]
-		if result != "ok" {
-			os.Exit(10)
-		}
-	}
+        if c.String("filter") == "" {
+            helpers.DisplayOutput(response, c.Bool("rawjson"))
+        } else {
+            filter := strings.Split(c.String("filter"), ",")
+            data := helpers.FilterProperties(response, filter)
+            helpers.DisplayOutput(data, c.Bool("rawjson"))
+        }
+        responseMap := helpers.ByteArrayToMap(response)
+        result := responseMap["result"]
+        if result != "ok" {
+            os.Exit(10)
+        }
+    }
 }
--- commands.go
+++ commands.go
@@ -1,99 +1,99 @@
 package main
 // https://github.com/home-assistant/hassio/blob/dev/API.md
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/home-assistant/hassio-cli/command"
-	"github.com/urfave/cli"
+    "github.com/home-assistant/hassio-cli/command"
+    "github.com/urfave/cli"
 )
 
 var GlobalFlags = []cli.Flag{}
 
 var Commands = []cli.Command{
-	{
-		Name:   "homeassistant",
-		Aliases:     []string{"ha"},
-		Usage:  "",
-		Action: command.CmdHomeassistant,
-		Flags:  []cli.Flag{
-			cli.BoolFlag{
-				Name: "rawjson, j",
-				Usage: "Returns the output in JSON format",
-			},
-			cli.StringFlag{
-				Name: "options, o",
-				Usage: "holds data for POST in format `key=val,key2=val2`",
-			},
-			cli.StringFlag{
-				Name: "filter, f",
-				Usage: "properties to extract from returned data `prop1,prop2`",
-			},
-		},
-	},
-	{
-		Name:   "supervisor",
-		Usage:  "",
-		Aliases: []string{"s"},
-		Action: command.CmdSupervisor,
-		Flags:  []cli.Flag{
-			cli.BoolFlag{
-				Name: "rawjson, j",
-				Usage: "Returns the output in JSON format",
-			},
-			cli.StringFlag{
-				Name: "options, o",
-				Usage: "holds data for POST in format `key=val,key2=val2`",
-			},
-			cli.StringFlag{
-				Name: "filter, f",
-				Usage: "properties to extract from returned data `prop1,prop2`",
-			},
-		},
-	},
-	{
-		Name:   "host",
-		Usage:  "",
-		Aliases: []string{"ho"},
-		Action: command.CmdHost,
-		Flags:  []cli.Flag{
-			cli.BoolFlag{
-				Name: "rawjson, j",
-				Usage: "Returns the output in JSON format",
-			},
-			cli.StringFlag{
-				Name: "options, o",
-				Usage: "holds data for POST in format `key=val,key2=val2`",
-			},
-			cli.StringFlag{
-				Name: "filter, f",
-				Usage: "properties to extract from returned data `prop1,prop2`",
-			},
-		},
-	},
-  {
-		Name:   "network",
-		Usage:  "",
-		Aliases: []string{"net"},
-		Action: command.CmdNetwork,
-		Flags:  []cli.Flag{
-			cli.BoolFlag{
-				Name: "rawjson, j",
-				Usage: "Returns the output in JSON format",
-			},
-			cli.StringFlag{
-				Name: "options, o",
-				Usage: "holds data for POST in format `key=val,key2=val2`",
-			},
-			cli.StringFlag{
-				Name: "filter, f",
-				Usage: "properties to extract from returned data `prop1,prop2`",
-			},
-		},
-	},
+    {
+        Name:   "homeassistant",
+        Aliases:     []string{"ha"},
+        Usage:  "",
+        Action: command.CmdHomeassistant,
+        Flags:  []cli.Flag{
+            cli.BoolFlag{
+                Name: "rawjson, j",
+                Usage: "Returns the output in JSON format",
+            },
+            cli.StringFlag{
+                Name: "options, o",
+                Usage: "holds data for POST in format `key=val,key2=val2`",
+            },
+            cli.StringFlag{
+                Name: "filter, f",
+                Usage: "properties to extract from returned data `prop1,prop2`",
+            },
+        },
+    },
+    {
+        Name:   "supervisor",
+        Usage:  "",
+        Aliases: []string{"s"},
+        Action: command.CmdSupervisor,
+        Flags:  []cli.Flag{
+            cli.BoolFlag{
+                Name: "rawjson, j",
+                Usage: "Returns the output in JSON format",
+            },
+            cli.StringFlag{
+                Name: "options, o",
+                Usage: "holds data for POST in format `key=val,key2=val2`",
+            },
+            cli.StringFlag{
+                Name: "filter, f",
+                Usage: "properties to extract from returned data `prop1,prop2`",
+            },
+        },
+    },
+    {
+        Name:   "host",
+        Usage:  "",
+        Aliases: []string{"ho"},
+        Action: command.CmdHost,
+        Flags:  []cli.Flag{
+            cli.BoolFlag{
+                Name: "rawjson, j",
+                Usage: "Returns the output in JSON format",
+            },
+            cli.StringFlag{
+                Name: "options, o",
+                Usage: "holds data for POST in format `key=val,key2=val2`",
+            },
+            cli.StringFlag{
+                Name: "filter, f",
+                Usage: "properties to extract from returned data `prop1,prop2`",
+            },
+        },
+    },
+    {
+        Name:   "network",
+        Usage:  "",
+        Aliases: []string{"net"},
+        Action: command.CmdNetwork,
+        Flags:  []cli.Flag{
+            cli.BoolFlag{
+                Name: "rawjson, j",
+                Usage: "Returns the output in JSON format",
+            },
+            cli.StringFlag{
+                Name: "options, o",
+                Usage: "holds data for POST in format `key=val,key2=val2`",
+            },
+            cli.StringFlag{
+                Name: "filter, f",
+                Usage: "properties to extract from returned data `prop1,prop2`",
+            },
+        },
+    },
 }
 
 func CommandNotFound(c *cli.Context, command string) {
-	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
-	os.Exit(2)
+    fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+    os.Exit(2)
 }
