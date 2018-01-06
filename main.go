44059bc9444d41ccda123db68ffd746e48dc3443
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-07 00:06:40 +0100

    wsBcBAABCAAQBQJaUVaACRBK7hj4Ov3rIwAAdHIIABgLGg1dKDrSyjSCSrpuOqvf  +Wmw3OKhhmNyx4AiX+4X9H73PfBtrh3tITkxk+U/nbX0hfbRsyBY13Beow6I0R/S  ZnjFMUW/orA2HaiJv4WIRQvh6lQA/rvn03aVs9gLvJGHl82sSP7WW2JikPCaFq/y  yWH3cfdWndvbG0MRNwwIgcRywEy+gWOYMXdyU/Nqc7OSZBpJ0eZPdDAFbAUnxCgn  sThhojex+s6pxnPYrfd93GcFhJ7FAtOoZ3JCDKrJ93H59lIbV/8pC1p6kbQo3oNq  TRwef7On0sPh22m4f96l+7MyRCd/U3HfYZ/zpl68f9yujhM15CH6TKt8OqmZh0A=  =0gGy  -----END PGP SIGNATURE-----
    fix spell

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app.Name = Name
 	app.Version = Version
 	app.Author = "Home-Assistant"
-	app.Email = "helo@home-assistant.io"
+	app.Email = "hello@home-assistant.io"
 	app.Usage = "Commandline tool to allow interation with hass.io"
 
 	app.Flags = GlobalFlags
