5bc72a1d08e6851f17e8fe72115b901591a04157
Cameron Moore <moorereason@gmail.com>
2018-03-02 10:57:40 -0600

    Use goimports

--- command/addons_test.go
+++ command/addons_test.go
@@ -3,5 +3,5 @@ package command
 import "testing"
 
 func TestCmdAddons(t *testing.T) {
-    // Write your code here
+	// Write your code here
 }
--- version.go
+++ version.go
@@ -2,5 +2,6 @@ package main
 
 // Name the name of the CLI
 const Name string = "hassio"
+
 // Version the current version of the CLI
 const Version string = "1.3.0Dev"
