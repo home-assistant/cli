a6d10ad405bc66657b3fbcc61aa15c25cc6df05f
Ryan McLean <ryanm101@users.noreply.github.com>
2018-04-17 12:22:48 +0100

    Release 1.3 (#74)
    * Set Version
    
    * Use GO Imports to force consistent formatting
    
    * Remove CHANGELOG (#67)
    
    Github releases provide the change log.
    
    * Set Version
    
    * Remove CHANGELOG (#67)
    
    Github releases provide the change log.
    
    * Revert "Use GO Imports to force consistent formatting"
    
    This reverts commit c81979032ba8a605917c3c6048b46e5aefd29df2.
    
    *  Added password & user options for snapshot create & restore (#71)
    
    Fixed snapshots to take -slug option instead of -name
    
    * Hardware API endpoint
    
    * Removed unused test files
    * Moved host/hardware to hardware/ per api change
    
    * Version Bump

--- commands.go
+++ commands.go
@@ -1,4 +1,5 @@
 package main
+
 // https://github.com/home-assistant/hassio/blob/dev/API.md
 import (
     "fmt"
@@ -11,7 +12,7 @@ import (
 // GlobalFlags Used to hold global flags
 var GlobalFlags = []cli.Flag{
     cli.BoolFlag{
-        Name: "debug, d",
+        Name:  "debug, d",
         Usage: "Prints Debug information",
     },
 }
@@ -19,129 +20,153 @@ var GlobalFlags = []cli.Flag{
 // Commands holds the commands that are supported by the CLI
 var Commands = []cli.Command{
     {
-        Name:   "homeassistant",
-        Aliases:     []string{"ha"},
-        Usage:  "info, logs, check, restart, start, stop, update",
-        Action: command.CmdHomeassistant,
-        Flags:  []cli.Flag{
+        Name:    "homeassistant",
+        Aliases: []string{"ha"},
+        Usage:   "info, logs, check, restart, start, stop, update",
+        Action:  command.CmdHomeassistant,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
         },
     },
     {
-        Name:   "supervisor",
-        Usage:  "info, logs, reload, update",
+        Name:    "supervisor",
+        Usage:   "info, logs, reload, update",
         Aliases: []string{"su"},
-        Action: command.CmdSupervisor,
-        Flags:  []cli.Flag{
+        Action:  command.CmdSupervisor,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
         },
     },
     {
-        Name:   "host",
-        Usage:  "hardware, reboot, shutdown, update",
+        Name:    "host",
+        Usage:   "reboot, shutdown, update",
         Aliases: []string{"ho"},
-        Action: command.CmdHost,
-        Flags:  []cli.Flag{
+        Action:  command.CmdHost,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
         },
     },
     {
-        Name:   "network",
-        Usage:  "info, options",
+        Name:    "hardware",
+        Usage:   "info, audio",
+        Aliases: []string{"hw"},
+        Action:  command.CmdHardware,
+        Flags: []cli.Flag{
+            cli.BoolFlag{
+                Name:  "rawjson, j",
+                Usage: "Returns the output in JSON format",
+            },
+            cli.StringFlag{
+                Name:  "filter, f",
+                Usage: "properties to extract from returned data `prop1,prop2`",
+            },
+        },
+    },
+    {
+        Name:    "network",
+        Usage:   "info, options",
         Aliases: []string{"ne"},
-        Action: command.CmdNetwork,
-        Flags:  []cli.Flag{
+        Action:  command.CmdNetwork,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
         },
     },
     {
-        Name:   "snapshots",
-        Usage:  "list, info, reload, new, restore, remove",
+        Name:    "snapshots",
+        Usage:   "list, info, reload, new, restore, remove",
         Aliases: []string{"sn"},
-        Action: command.CmdSnapshots,
-        Flags:  []cli.Flag{
+        Action:  command.CmdSnapshots,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
             cli.StringFlag{
-                Name: "name",
-                Usage: "used with 'info|remove|restore|full' actions to return info on a specific snapshot `nameofsnapshot`",
+                Name:  "slug",
+                Usage: "used with 'info|remove|restore|new' actions to return info on a specific snapshot `slugofsnapshot`",
+            },
+            cli.StringFlag{
+                Name:  "name",
+                Usage: "used with 'restore|new' actions to set a name for a snapshot",
+            },
+            cli.StringFlag{
+                Name:  "password",
+                Usage: "used with 'restore|new' actions to set a password on a snapshot",
             },
         },
     },
     {
-        Name:   "addons",
-        Usage:  "list, info, logo, changelog, logs, stats,\n reload, start, stop, install, uninstall, update",
+        Name:    "addons",
+        Usage:   "list, info, logo, changelog, logs, stats,\n reload, start, stop, install, uninstall, update",
         Aliases: []string{"ad"},
-        Action: command.CmdAddons,
-        Flags:  []cli.Flag{
+        Action:  command.CmdAddons,
+        Flags: []cli.Flag{
             cli.BoolFlag{
-                Name: "rawjson, j",
+                Name:  "rawjson, j",
                 Usage: "Returns the output in JSON format",
             },
             cli.StringFlag{
-                Name: "options, o",
+                Name:  "options, o",
                 Usage: "holds data for POST in format `key=val,key2=val2`",
             },
             cli.StringFlag{
-                Name: "filter, f",
+                Name:  "filter, f",
                 Usage: "properties to extract from returned data `prop1,prop2`",
             },
             cli.StringFlag{
-                Name: "name",
+                Name:  "name",
                 Usage: "used with 'info' actions to return info on a specific addon `nameofaddon`",
             },
         },
