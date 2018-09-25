79e37c55e560da7bae29b42c5496c63136cfda33
Robbert Müller <spam.me@grols.ch>
2018-09-25 20:08:46 +0200

    Added basic logging setup

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
 func main() {
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
 
 	app := cli.NewApp()
 	app.Name = Name
@@ -17,6 +21,12 @@ func main() {
 
 	app.Flags = GlobalFlags
 	app.Commands = Commands
+	app.Before = func(c *cli.Context) error {
+		if c.GlobalBool("debug") {
+			log.SetLevel(log.DebugLevel)
+		}
+		return nil
+	}
 	app.CommandNotFound = CommandNotFound
 
 	app.Run(os.Args)
