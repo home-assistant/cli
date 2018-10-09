fc8e4f8f4264a5fbc5d27d4d3790a4eff2b67fcc
Bren Briggs <code@fraq.io>
2018-10-09 01:41:16 +0000

    Add comment for addons.Execute()

--- cmd/addons/root.go
+++ cmd/addons/root.go
@@ -15,6 +15,7 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// Execute represents the entrypoint for subcommands associated with "addons"
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
