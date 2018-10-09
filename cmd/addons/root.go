0d17138bac315cae76579aaa4f98fe47979de98a
Bren Briggs <code@fraq.io>
2018-10-09 01:08:22 +0000

    addons subcommand started

--- cmd/addons/root.go
+++ cmd/addons/root.go
@@ -1,22 +1,17 @@
-package cmd
+package addons
 
 import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "hassio",
-	Short: "A brief description of your application",
+	Use:   "addons",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hassio")
+		fmt.Println("addons sub-command")
 	},
 }
 
@@ -24,37 +19,5 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.homeassistant.yaml)")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".homeassistant" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".homeassistant.yaml")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
