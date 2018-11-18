279f48b7773146efa4f0dac30595ae0b22925cdb
Robbert Müller <spam.me@grols.ch>
2018-11-18 18:15:39 +0100

    Added supervisor update command

---
+++ cmd/supervisor_update.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/hassio-cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var supervisorVersion = ""
+
+// updateCmd represents the update command
+var supervisorUpdateCmd = &cobra.Command{
+	Use:     "update",
+	Aliases: []string{"up"},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("supervisor update")
+
+		section := "supervisor"
+		command := "update"
+		base := viper.GetString("endpoint")
+
+		url, err := helper.URLHelper(base, section, command)
+		if err != nil {
+			// TODO: error handler
+			fmt.Printf("Error: %v", err)
+			return
+		}
+
+		request := helper.GetJSONRequest()
+
+		// TODO: submit version
+		if supervisorVersion != "" {
+			request.SetBody(map[string]interface{}{"version": supervisorVersion})
+		}
+
+		resp, err := request.Post(url)
+
+		// returns 200 OK or 400
+		if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+			fmt.Println("Unexpected server response")
+			fmt.Println(resp.String())
+		} else {
+			helper.ShowJSONResponse(resp)
+		}
+
+		return
+	},
+}
+
+func init() {
+	supervisorUpdateCmd.Flags().StringVarP(&supervisorVersion, "version", "", "", "Version to update to")
+	supervisorCmd.AddCommand(supervisorUpdateCmd)
+}
