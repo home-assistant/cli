2c870068fe41126cfa68dbb1a195999116ee59a9
Robbert Müller <spam.me@grols.ch>
2018-11-18 18:15:20 +0100

    Added supervior logs command

---
+++ cmd/supervisor_logs.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/hassio-cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// infoCmd represents the info command
+var supervisorLogsCmd = &cobra.Command{
+	Use:     "logs",
+	Aliases: []string{"lo"},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("supervisor logs")
+
+		section := "supervisor"
+		command := "logs"
+		base := viper.GetString("endpoint")
+
+		url, err := helper.URLHelper(base, section, command)
+		if err != nil {
+			// TODO: error handler
+			fmt.Printf("Error: %v", err)
+			return
+		}
+
+		request := helper.GetRequest()
+		resp, err := request.SetHeader("Accept", "text/plain").Get(url)
+
+		fmt.Println(resp.String())
+		return
+	},
+}
+
+func init() {
+	supervisorCmd.AddCommand(supervisorLogsCmd)
+}
