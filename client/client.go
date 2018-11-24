de3f9e2d062980c2e6722861bd535f47522861f5
Robbert Müller <spam.me@grols.ch>
2018-11-24 15:03:00 +0100

    Added a generic GET method

--- client/client.go
+++ client/client.go
@@ -8,21 +8,24 @@ import (
 	resty "gopkg.in/resty.v1"
 )
 
-// GenericJSONPost is a helper for generic empty post request
-func GenericJSONPost(base, section, command string, body map[string]interface{}) (*resty.Response, error) {
+func genericJSONMethod(get bool, base, section, command string, body map[string]interface{}) (*resty.Response, error) {
 	url, err := URLHelper(base, section, command)
 	if err != nil {
 		return nil, err
 	}
 
 	request := GetJSONRequest()
+	var resp *resty.Response
 
-	if body != nil && len(body) > 0 {
-		log.WithField("body", body).Debug("Request body")
-		request.SetBody(body)
+	if get == true {
+		resp, err = request.Get(url)
+	} else {
+		if body != nil && len(body) > 0 {
+			log.WithField("body", body).Debug("Request body")
+			request.SetBody(body)
+		}
+		resp, err = request.Post(url)
 	}
-
-	resp, err := request.Post(url)
 
 	// returns 200 OK or 400, everything else is wrong
 	if err == nil {
@@ -37,4 +40,14 @@ func GenericJSONPost(base, section, command string, body map[string]interface{})
 		}
 	}
 	return resp, nil
+}
+
+// GenericJSONGet is a helper for generic empty post request
+func GenericJSONGet(base, section, command string) (*resty.Response, error) {
+	return genericJSONMethod(true, base, section, command, nil)
+}
+
+// GenericJSONPost is a helper for generic empty post request
+func GenericJSONPost(base, section, command string, body map[string]interface{}) (*resty.Response, error) {
+	return genericJSONMethod(false, base, section, command, body)
 }
