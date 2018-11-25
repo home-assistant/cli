c72b1af8927848e346e609bd9acc17045304ddc9
Robbert Müller <spam.me@grols.ch>
2018-11-25 16:13:55 +0100

    Added snapshots reload

---
+++ cmd/snapshots_reload.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/hassio-cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// reloadCmd represents the reload command
+var snapshotsReloadCmd = &cobra.Command{
+	Use:     "reload",
+	Aliases: []string{"re"},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("snapshots reload")
+
+		section := "snapshots"
+		command := "reload"
+		base := viper.GetString("endpoint")
+
+		resp, err := helper.GenericJSONPost(base, section, command, nil)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			helper.ShowJSONResponse(resp)
+		}
+
+		return
+	},
+}
+
+func init() {
+	snapshotsCmd.AddCommand(snapshotsReloadCmd)
+}
