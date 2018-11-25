0530b087d6bd271ba4485aad14be20aef3e99dda
Robbert Müller <spam.me@grols.ch>
2018-11-25 17:37:48 +0100

    Added snapshot info
    No helpers used because of the custom client initialisation

---
+++ cmd/snapshots_info.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	helper "github.com/home-assistant/hassio-cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	resty "gopkg.in/resty.v1"
+)
+
+// reloadCmd represents the info command
+var snapshotsInfoCmd = &cobra.Command{
+	Use:     "info",
+	Aliases: []string{"re"},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("snapshots info")
+
+		section := "snapshots"
+		command := "{slug}/info"
+		base := viper.GetString("endpoint")
+
+		url, err := helper.URLHelper(base, section, command)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		request := helper.GetJSONRequest()
+
+		slug, _ := cmd.Flags().GetString("slug")
+
+		request.SetPathParams(map[string]string{
+			"slug": slug,
+		})
+
+		resp, err := request.Get(url)
+
+		// returns 200 OK or 400, everything else is wrong
+		if err == nil {
+			if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+				err = errors.New("Unexpected server response")
+				log.Error(err)
+			} else if !resty.IsJSONType(resp.Header().Get(http.CanonicalHeaderKey("Content-Type"))) {
+				err = errors.New("api did not return a json response")
+				log.Error(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			helper.ShowJSONResponse(resp)
+		}
+
+		return
+	},
+}
+
+func init() {
+	snapshotsInfoCmd.Flags().StringP("slug", "", "", "Slug of the snapshot")
+	snapshotsInfoCmd.MarkFlagRequired("slug")
+
+	snapshotsCmd.AddCommand(snapshotsInfoCmd)
+}
