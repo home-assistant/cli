3c660d52d80b2c7a121d83ae664d7842f765caa9
Robbert Müller <spam.me@grols.ch>
2018-11-27 19:29:56 +0100

    Added addon list

--- cmd/addons.go
+++ cmd/addons.go
@@ -1,20 +1,36 @@
 package cmd
 
 import (
+	"fmt"
+
+	helper "github.com/home-assistant/hassio-cli/client"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var addonsCmd = &cobra.Command{
 	Use:     "addons",
 	Aliases: []string{"ad"},
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("addons")
+
+		section := "addons"
+		command := ""
+		base := viper.GetString("endpoint")
+
+		resp, err := helper.GenericJSONGet(base, section, command)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			helper.ShowJSONResponse(resp)
+		}
+		return
+	},
 }
 
 func init() {
 	log.Debug("Init addons")
-	// add subcommands
-	// TODO: add subcommand
 
-	// add cmd to root command
 	rootCmd.AddCommand(addonsCmd)
 }
