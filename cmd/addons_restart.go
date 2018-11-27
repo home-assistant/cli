0687fa2a49061c9160963442dec0c1ff52066edf
Robbert Müller <spam.me@grols.ch>
2018-11-27 21:11:05 +0100

    Added addons start/stop/restart/install/uninstall/update/rebuild

---
+++ cmd/addons_restart.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	helper "github.com/home-assistant/hassio-cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	resty "gopkg.in/resty.v1"
+)
+
+var addonsRestartCmd = &cobra.Command{
+	Use:  "restart [slug]",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("addons restart")
+
+		section := "addons"
+		command := "{slug}/restart"
+		base := viper.GetString("endpoint")
+
+		url, err := helper.URLHelper(base, section, command)
+		fmt.Println(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		request := helper.GetJSONRequest()
+
+		slug := args[0]
+
+		request.SetPathParams(map[string]string{
+			"slug": slug,
+		})
+
+		resp, err := request.Post(url)
+
+		// returns 200 OK or 400, everything else is wrong
+		if err == nil {
+			if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+				err = errors.New("Unexpected server response")
+				log.Error(err)
+			} else if !resty.IsJSONType(resp.Header().Get(http.CanonicalHeaderKey("Content-Type"))) {
+				err = errors.New("api did not return a json response")
+				log.Error(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			helper.ShowJSONResponse(resp)
+		}
+
+		return
+	},
+}
+
+func init() {
+
+	addonsCmd.AddCommand(addonsRestartCmd)
+}
