e6f93e88d9ed0a411c4dec9cb0196f6565080462
Franck Nijhof <frenck@addons.community>
2019-08-13 21:15:14 +0200

    Improves info command documentation and aliasses

--- cmd/info.go
+++ cmd/info.go
@@ -13,7 +13,16 @@ import (
 
 var infoCmd = &cobra.Command{
 	Use:     "info",
-	Aliases: []string{"in"},
+	Aliases: []string{"in", "inf"},
+	Short: 	 "Provides a general Hass.io information overview",
+	Long: `
+This command provide a general information about your Hass.io system.
+The information provide can be useful for sharing when you are encountering
+issues or when reporting one on GitHub.
+	`,
+	Example: `
+  hassio info
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.WithField("args", args).Debug("info")
 
