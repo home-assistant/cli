65d7481f7b0dd46cafe534a49eb18bd331b3309e
Franck Nijhof <frenck@addons.community>
2019-08-13 22:23:28 +0200

    Improves host command documentation and aliasses

--- cmd/host.go
+++ cmd/host.go
@@ -7,6 +7,15 @@ import (
 var hostCmd = &cobra.Command{
 	Use:     "host",
 	Aliases: []string{"ho"},
+	Short:   "Control the host/system that Hass.io is running on",
+	Long: `
+The host command provides commandline tools to control the host (system) that
+Hass.io is running on. It allows you do thing like reboot or shutdown the
+system, but also provides option to change the hostname of the system.`,
+	Example: `
+  hassio host reboot
+  hassio host options --hostname "homeassistant.local"
+`,
 }
 
 func init() {
