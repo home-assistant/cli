87bd86909b4daa2e8956f7cb0515b546c8cfacbb
Franck Nijhof <frenck@frenck.nl>
2019-08-15 13:02:11 +0200

    Fixes some typos in CLI help (#176)

--- cmd/dns.go
+++ cmd/dns.go
@@ -8,12 +8,12 @@ var dnsCmd = &cobra.Command{
 	Use:     "dns",
 	Short:   "Get information, update or configure the Hass.io DNS server",
 	Long: `
-The dbs command allows you to manage the internal Hass.io DNS server by
+The dns command allows you to manage the internal Hass.io DNS server by
 exposing commands to view, monitor, configure and control it.`,
 	Example: `
   hassio dns logs
   hassio dns info
-  hassio dns upgrade`,
+  hassio dns update`,
 }
 
 func init() {
