94adaa7f714e09c6d1e635bfea75b8ce58d50b06
Franck Nijhof <git@frenck.dev>
2020-02-10 16:16:03 +0100

    Fix execution of rootCmd PreRun (#200)

--- cmd/core.go
+++ cmd/core.go
@@ -25,6 +25,7 @@ Home Assistant Core.
   ha core update
 	ha core update --version 0.97.2`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		rootCmd.PersistentPreRun(cmd, args)
 		for _, arg := range os.Args {
 			if arg == "homeassistant" || arg == "ha" {
 				cmd.PrintErrf("The use of '%s' is deprecated, please use 'core' instead!\n", arg)
