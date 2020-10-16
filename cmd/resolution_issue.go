8231124f1d1872d734aec356f8d169814037d543
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-16 17:30:43 +0200

    Update cmd/resolution_issue.go
    Co-authored-by: Joakim Sørensen <joasoe@gmail.com>

--- cmd/resolution_issue.go
+++ cmd/resolution_issue.go
@@ -9,7 +9,7 @@ var resolutionIssueCmd = &cobra.Command{
 	Aliases: []string{"is", "trouble"},
 	Short:   "Issues management reported by Resolution center",
 	Long: `
-This command allow to dismiss issues reported by the System.`,
+This command allows dismissing issues reported by the system.`,
 	Example: `
   ha resolution issue dismiss [id]`,
 }
