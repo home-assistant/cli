5416d43797bda395ffc86f1d384bbf1d1a3632e3
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-19 16:11:55 +0000

    Add auth cache command

---
+++ cmd/auth_cache.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	resty "github.com/go-resty/resty/v2"
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var authCacheCmd = &cobra.Command{
+	Use:     "cache",
+	Aliases: []string{"data", "ca"},
+	Short:   "Reset the auth cache of Home Assistant on Supervisor.",
+	Long: `
+This command allows you to reset the internal password cache of a Home Assistant auth.
+`,
+	Example: `
+  ha auth cache`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("auth cache")
+
+		section := "auth"
+		command := "cache"
+		base := viper.GetString("endpoint")
+
+		url, err := helper.URLHelper(base, section, command)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+			return
+		}
+
+		request := helper.GetJSONRequest()
+
+		resp, err := request.Delete(url)
+
+		// returns 200 OK or 400, everything else is wrong
+		if err == nil {
+			if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+				err = errors.New("Unexpected server response")
+				log.Error(err)
+			} else if !resty.IsJSONType(resp.Header().Get(http.CanonicalHeaderKey("Content-Type"))) {
+				err = errors.New("API did not return a JSON response")
+				log.Error(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+
+		return
+	},
+}
+
+func init() {
+	authCmd.AddCommand(authCacheCmd)
+}
