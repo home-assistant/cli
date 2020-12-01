163495419e8ed0970a3e9eee5532c35bf3b91066
Pascal Vizeli <pvizeli@syshack.ch>
2020-12-01 13:02:47 +0000

    Network reload command

---
+++ cmd/network_reload.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var networkReloadCmd = &cobra.Command{
+	Use:     "reload",
+	Aliases: []string{"re", "update"},
+	Short:   "Reload Network information the host",
+	Long: `
+Reload information about the host network and interfaces.
+`,
+	Example: `
+  ha network reload
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("network reload")
+
+		section := "network"
+		command := "reload"
+		base := viper.GetString("endpoint")
+
+		resp, err := helper.GenericJSONPost(base, section, command, nil)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	networkCmd.AddCommand(networkReloadCmd)
+}
