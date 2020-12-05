5da962c66878fe840fb67e2a0bff1ea1e87adb27
Stefan Agner <stefan@agner.ch>
2020-12-05 18:55:43 +0100

    Print status code if unexpected response is received (#214) (#267)

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"errors"
+	"fmt"
 
 	resty "github.com/go-resty/resty/v2"
 	log "github.com/sirupsen/logrus"
@@ -32,7 +33,7 @@ func genericJSONMethod(get bool, base, section, command string, body map[string]
 	// returns 200 OK or 400, everything else is wrong
 	if err == nil {
 		if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
-			err = errors.New("Unexpected server response")
+			err = fmt.Errorf("Unexpected server response. Status code: %d", resp.StatusCode())
 			log.Error(err)
 			return nil, err
 		} else if !resty.IsJSONType(resp.Header().Get("Content-Type")) {
