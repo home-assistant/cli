aa74307ec15e25f66d65e7e63d5c2936b68b8d11
Pascal Vizeli <pvizeli@syshack.ch>
2021-03-24 14:36:43 +0100

    Add force-security options to supervisor (#285)

--- cmd/supervisor_options.go
+++ cmd/supervisor_options.go
@@ -45,6 +45,7 @@ Supervisor running on your Home Assistant system.`,
 			"debug-block",
 			"diagnostics",
 			"content-trust",
+			"force-security",
 		} {
 			data, err := cmd.Flags().GetBool(value)
 			if err == nil && cmd.Flags().Changed(value) {
@@ -81,12 +82,14 @@ func init() {
 	supervisorOptionsCmd.Flags().StringP("logging", "l", "", "Logging: debug|info|warning|error|critical")
 	supervisorOptionsCmd.Flags().IntP("wait-boot", "w", 0, "Seconds to wait after boot")
 	supervisorOptionsCmd.Flags().BoolP("content-trust", "", true, "Enable/Disable content-trust on the backend")
+	supervisorOptionsCmd.Flags().BoolP("force-security", "", false, "Enable/Disable force-security on the backend")
 	supervisorOptionsCmd.Flags().BoolP("debug", "", false, "Enable debug mode")
 	supervisorOptionsCmd.Flags().BoolP("debug-block", "", false, "Enable debug mode with blocking startup")
 	supervisorOptionsCmd.Flags().BoolP("diagnostics", "", false, "Enable diagnostics mode")
 	supervisorOptionsCmd.Flags().StringArrayP("repositories", "r", []string{}, "repositories to track, can be supplied multiple times")
 
 	supervisorOptionsCmd.Flags().Lookup("content-trust").NoOptDefVal = "true"
+	supervisorOptionsCmd.Flags().Lookup("force-security").NoOptDefVal = "false"
 	supervisorOptionsCmd.Flags().Lookup("debug").NoOptDefVal = "false"
 	supervisorOptionsCmd.Flags().Lookup("debug-block").NoOptDefVal = "false"
 	supervisorOptionsCmd.Flags().Lookup("diagnostics").NoOptDefVal = "false"
