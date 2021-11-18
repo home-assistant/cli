22b0d1798b05af105046e1e513ccd6e726ec41e2
Joakim Sørensen <joasoe@gmail.com>
2021-11-18 15:20:49 +0100

    Add available_updates command to supervisor (#319)
    Co-authored-by: Paulus Schoutsen <balloob@gmail.com>

---
+++ cmd/supervisor_available_updates.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var supervisorAvailableUpdatesCmd = &cobra.Command{
+	Use:     "available_updates",
+	Aliases: []string{"updates"},
+	Short:   "Provides information about available updates",
+	Long: `
+This command provides you information about available updates.`,
+	Example: `
+  ha supervisor available_updates`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("supervisor available_updates")
+
+		section := "supervisor"
+		command := "available_updates"
+		base := viper.GetString("endpoint")
+
+		resp, err := helper.GenericJSONGet(base, section, command)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+
+	},
+}
+
+func init() {
+	supervisorCmd.AddCommand(supervisorAvailableUpdatesCmd)
+}
