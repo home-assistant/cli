59a0ef39666001ccaa2d6ff5e72cde9d37a6aa84
Pascal Vizeli <pascal.vizeli@syshack.ch>
2021-12-28 12:56:54 +0100

    fix lint

--- cmd/supervisor_available_updates.go
+++ cmd/supervisor_available_updates.go
@@ -6,7 +6,6 @@ import (
 	helper "github.com/home-assistant/cli/client"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var supervisorAvailableUpdatesCmd = &cobra.Command{
