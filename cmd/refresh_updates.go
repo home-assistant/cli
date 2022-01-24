b4f52aa41888f9386dbf5892d71d0562db26b819
Ludeeus <ludeeus@ludeeus.dev>
2022-01-24 09:19:12 +0000

    Add new root endpoints

---
+++ cmd/refresh_updates.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var refreshUpdatesCmd = &cobra.Command{
+	Use:     "refresh-updates",
+	Aliases: []string{"refresh", "refresh_updates"},
+	Short:   "Reload stores and version information",
+	Long: `
+This command reloads information about add-on repositories and fetches new version files.
+	`,
+	Example: `
+  ha refresh-update
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("refresh_updates")
+
+		section := "refresh_updates"
+		command := ""
+
+		resp, err := helper.GenericJSONPost(section, command, nil)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(refreshUpdatesCmd)
+}
