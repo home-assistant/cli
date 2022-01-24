bdae1c837c5b6779fa0463d6f755bc50da56dde6
Joakim Sørensen <joasoe@gmail.com>
2022-01-24 14:51:39 +0100

    Add new root endpoints (#331)

---
+++ cmd/available_updates.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var availableUpdatesCmd = &cobra.Command{
+	Use:     "available-updates",
+	Aliases: []string{"updates", "available_updates"},
+	Short:   "Provides information about current pending updates",
+	Long: `
+This command provides information about the currently pending updates on the system.
+	`,
+	Example: `
+  ha available-updates
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("available_updates")
+
+		section := "available_updates"
+		command := ""
+
+		resp, err := helper.GenericJSONGet(section, command)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(availableUpdatesCmd)
+}
