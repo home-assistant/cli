850283d9018a43c5c4ded19532dd53aa01213646
Ville Skyttä <ville.skytta@iki.fi>
2022-08-17 07:07:00 +0300

    Add store addons completion

--- cmd/store_addons.go
+++ cmd/store_addons.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	helper "github.com/home-assistant/cli/client"
 	log "github.com/sirupsen/logrus"
@@ -36,4 +37,62 @@ commands for installing or update them.`,
 
 func init() {
 	storeCmd.AddCommand(storeAddonsCmd)
+}
+
+func storeAddonCompletions(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) != 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	resp, err := helper.GenericJSONGet("store", "")
+	if err != nil || !resp.IsSuccess() {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	var ret []string
+	data := resp.Result().(*helper.Response)
+	if data.Result == "ok" && data.Data["addons"] != nil {
+		if addons, ok := data.Data["addons"].([]interface{}); ok {
+			for _, addon := range addons {
+				var m map[string]interface{}
+				if m, ok = addon.(map[string]interface{}); !ok {
+					continue
+				}
+				var s string
+				if s, ok = m["slug"].(string); !ok {
+					continue
+				}
+				var b bool
+				switch cmd.Name() {
+				case "install":
+					if b, ok = m["available"].(bool); ok && !b {
+						continue
+					}
+					if b, ok = m["installed"].(bool); ok && b {
+						continue
+					}
+				case "update":
+					if b, ok = m["available"].(bool); ok && !b {
+						continue
+					}
+					if b, ok = m["installed"].(bool); ok && !b {
+						continue
+					}
+					if b, ok = m["update_available"].(bool); ok && !b {
+						continue
+					}
+				}
+				ret = append(ret, s)
+				var ds []string
+				if s, ok = m["name"].(string); ok && s != "" {
+					ds = append(ds, s)
+				}
+				if s, ok = m["url"].(string); ok && s != "" {
+					ds = append(ds, s)
+				}
+				if len(ds) != 0 {
+					ret[len(ret)-1] += "\t" + strings.Join(ds, ", ")
+				}
+			}
+		}
+	}
+	return ret, cobra.ShellCompDirectiveNoFileComp
 }
