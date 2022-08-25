281890394b7e2a56e59694410667bf9fbfe60c85
Mike Degatano <michael.degatano@gmail.com>
2022-08-24 20:25:15 -0400

    Add issue suggestions API to CLI

---
+++ cmd/resolution_issue_suggestions.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	resty "github.com/go-resty/resty/v2"
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var resolutionIssueSuggestionsCmd = &cobra.Command{
+	Use:     "suggestions",
+	Aliases: []string{"su", "solutions"},
+	Short:   "Suggestions which resolve an issue",
+	Long: `
+This command returns suggestions which resolve an issue when applied.`,
+	Example: `
+  ha resolution issue suggestions [id]`,
+	ValidArgsFunction: cobra.NoFileCompletions,
+	Args:              cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("issue suggestions")
+
+		section := "resolution"
+		command := "issue/{issue}/suggestions"
+
+		url, err := helper.URLHelper(section, command)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+			return
+		}
+
+		request := helper.GetJSONRequest()
+
+		issue := args[0]
+
+		request.SetPathParams(map[string]string{
+			"issue": issue,
+		})
+
+		resp, err := request.Get(url)
+
+		// returns 200 OK or 400, everything else is wrong
+		if err == nil {
+			if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+				err = errors.New("Unexpected server response")
+				log.Error(err)
+			} else if !resty.IsJSONType(resp.Header().Get("Content-Type")) {
+				err = errors.New("API did not return a JSON response")
+				log.Error(err)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	resolutionCmd.AddCommand(resolutionIssueSuggestionsCmd)
+}
