bcf8421c8b6e3c1d45d7361137cbf9f7cf1165ca
Ville Skyttä <ville.skytta@iki.fi>
2022-10-19 09:40:11 +0300

    Add resolution issue completions

--- cmd/resolution_issue.go
+++ cmd/resolution_issue.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+
+	helper "github.com/home-assistant/cli/client"
 )
 
 var resolutionIssueCmd = &cobra.Command{
@@ -16,4 +18,35 @@ This command allows dismissing issues reported by the system.`,
 
 func init() {
 	resolutionCmd.AddCommand(resolutionIssueCmd)
+}
+
+func resolutionIssueCompletions(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) != 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	resp, err := helper.GenericJSONGet("resolution", "info")
+	if err != nil || !resp.IsSuccess() {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	var ret []string
+	data := resp.Result().(*helper.Response)
+	if data.Result == "ok" && data.Data["issues"] != nil {
+		if issues, ok := data.Data["issues"].([]interface{}); ok {
+			for _, issue := range issues {
+				var m map[string]interface{}
+				if m, ok = issue.(map[string]interface{}); !ok {
+					continue
+				}
+				var s string
+				if s, ok = m["uuid"].(string); !ok {
+					continue
+				}
+				if t, ok := m["type"].(string); ok {
+					s += "\t" + t
+				}
+				ret = append(ret, s)
+			}
+		}
+	}
+	return ret, cobra.ShellCompDirectiveNoFileComp
 }
--- cmd/resolution_issue_dismiss.go
+++ cmd/resolution_issue_dismiss.go
@@ -18,7 +18,7 @@ var resolutionIssueDismissCmd = &cobra.Command{
 This command allows dismissing issues reported by the system.`,
 	Example: `
   ha resolution issue dismiss [id]`,
-	ValidArgsFunction: cobra.NoFileCompletions,
+	ValidArgsFunction: resolutionIssueCompletions,
 	Args:              cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.WithField("args", args).Debug("issue dismiss")
