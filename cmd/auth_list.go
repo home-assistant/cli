cef08dc8f50ff065a889355c7760e3e1e7751f27
Mike Degatano <michael.degatano@gmail.com>
2024-03-01 09:35:41 -0500

    Allow listing of HA users via admin CLI (#462)

---
+++ cmd/auth_list.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var authListCmd = &cobra.Command{
+	Use:     "list",
+	Aliases: []string{"ls"},
+	Short:   "List all Home Assistant users.",
+	Long: `
+This command allows you to list all Home Assistant users on the system.
+Please note, this command is limited due to security reasons, and will
+only work on some locations. For example, the Operating System CLI.
+`,
+	Example: `
+  ha authentication list
+`,
+	ValidArgsFunction: cobra.NoFileCompletions,
+	Args:              cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("auth list")
+
+		section := "auth"
+		command := "list"
+
+		resp, err := helper.GenericJSONGet(section, command)
+		if err != nil {
+			cmd.PrintErrln("this command is limited due to security reasons, and will only work on some locations. For example, the Operating System terminal.")
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	authCmd.AddCommand(authListCmd)
+}
