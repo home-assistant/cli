2fcd2a51fde9429d2c72c397e8fc4fc38b91a888
Mike Degatano <michael.degatano@gmail.com>
2024-04-04 02:48:50 -0400

    Allow changing the boot slot via cli (#469)
    * Allow changing the boot slot via cli
    
    * Fix linting errors
    
    * Fix additional lint issues
    
    * Prealloc array for perf
    
    * Improve boot slot completions
    
    * Switch from rollback to boot-slot other

--- cmd/os.go
+++ cmd/os.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"os"
+	"strings"
+
+	helper "github.com/home-assistant/cli/client"
 
 	"github.com/spf13/cobra"
 )
@@ -31,4 +34,58 @@ it provides a command to import configurations from a USB stick.`,
 
 func init() {
 	rootCmd.AddCommand(osCmd)
+}
+
+func osGetBootSlots() (map[string]interface{}, error) {
+	resp, err := helper.GenericJSONGet("os", "info")
+	if err != nil || !resp.IsSuccess() {
+		return nil, err
+	}
+
+	data := resp.Result().(*helper.Response)
+	if data.Result == "ok" && data.Data["boot_slots"] != nil {
+		if bootSlots, ok := data.Data["boot_slots"].(map[string]interface{}); ok {
+			return bootSlots, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func osBootSlotCompletions(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) != 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	bootSlots, _ := osGetBootSlots()
+	if bootSlots != nil {
+		ret := make([]string, 2)
+		for bootSlot, v := range bootSlots {
+			info, ok := v.(map[string]interface{})
+			if !ok {
+				continue
+			}
+			if state, ok := info["state"].(string); !ok || state == "active" {
+				continue
+			}
+
+			ret[0] = "other"
+			ret[1] = bootSlot
+			var details []string
+			if version, ok := info["version"].(string); ok && version != "" {
+				details = append(details, version)
+			}
+			if status, ok := info["status"].(string); ok && status != "" {
+				details = append(details, status)
+			}
+
+			if len(details) > 0 {
+				detailsStr := "\t" + strings.Join(details, ", ")
+				ret[0] += detailsStr
+				ret[1] += detailsStr
+			}
+			return ret, cobra.ShellCompDirectiveNoFileComp
+		}
+	}
+	return nil, cobra.ShellCompDirectiveNoFileComp
 }
---
+++ cmd/os_boot_slot.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"fmt"
+
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var osBootSlotCmd = &cobra.Command{
+	Use:     "boot-slot [slot]",
+	Aliases: []string{"boot"},
+	Short:   "Changes the active boot slot",
+	Long: `
+Using this command you can change the active boot slot to rollback
+an OS update without making more changes to the system.
+`,
+	Example: `
+  ha os boot-slot other
+  ha os boot-slot A
+`,
+	ValidArgsFunction: osBootSlotCompletions,
+	Args:              cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("os boot-slot")
+
+		section := "os"
+		command := "boot-slot"
+
+		bootSlot := args[0]
+		if bootSlot == "other" {
+			bootSlots, err := osGetBootSlots()
+			if err != nil {
+				fmt.Println(err)
+				ExitWithError = true
+				return
+			}
+			if bootSlots == nil {
+				ExitWithError = true
+				return
+			}
+
+			for name, v := range bootSlots {
+				info, ok := v.(map[string]interface{})
+				if !ok {
+					continue
+				}
+				if state, ok := info["state"].(string); ok && state == "inactive" {
+					bootSlot = name
+					break
+				}
+			}
+			if bootSlot == "other" {
+				ExitWithError = true
+				return
+			}
+		}
+
+		options := map[string]interface{}{"boot_slot": bootSlot}
+		resp, err := helper.GenericJSONPost(section, command, options)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	osCmd.AddCommand(osBootSlotCmd)
+}
