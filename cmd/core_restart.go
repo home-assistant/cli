cdd73d838c85919a5dfa55a19a7f101d4f985b13
Mike Degatano <michael.degatano@gmail.com>
2024-04-12 17:02:55 -0400

    Allow restarting core in safe mode

--- cmd/core_restart.go
+++ cmd/core_restart.go
@@ -24,18 +24,41 @@ Restart the Home Assistant Core instance running on your system`,
 		section := "core"
 		command := "restart"
 
-		ProgressSpinner.Start()
-		resp, err := helper.GenericJSONPostTimeout(section, command, nil, helper.ContainerOperationTimeout)
-		ProgressSpinner.Stop()
+		url, err := helper.URLHelper(section, command)
 		if err != nil {
 			fmt.Println(err)
 			ExitWithError = true
-		} else {
-			ExitWithError = !helper.ShowJSONResponse(resp)
+			return
+		}
+
+		request := helper.GetRequestTimeout(helper.ContainerOperationTimeout)
+
+		safeMode, err := cmd.Flags().GetBool("safe-mode")
+		if err == nil && safeMode {
+			options := make(map[string]interface{})
+			options["safe_mode"] = safeMode
+			request.SetBody(options)
+		}
+
+		ProgressSpinner.Start()
+		resp, err := request.Post(url)
+		ProgressSpinner.Stop()
+
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
+			err = fmt.Errorf("Unexpected server response. Status code: %d", resp.StatusCode())
+			log.Error(err)
+			ExitWithError = true
 		}
 	},
 }
 
 func init() {
+	coreRestartCmd.Flags().BoolP("safe-mode", "s", false, "Restart Home Assistant in safe mode")
+	coreRestartCmd.Flags().Lookup("safe-mode").NoOptDefVal = "true"
+	coreRestartCmd.RegisterFlagCompletionFunc("safe-mode", boolCompletions)
+
 	coreCmd.AddCommand(coreRestartCmd)
 }
