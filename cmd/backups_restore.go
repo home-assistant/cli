1f423ca781a2120c63a0036a862a1b5cb23e7c63
Mike Degatano <michael.degatano@gmail.com>
2024-07-30 16:35:20 -0400

    Centralize error handling and add 503 support

--- cmd/backups_restore.go
+++ cmd/backups_restore.go
@@ -1,10 +1,9 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
 
-	resty "github.com/go-resty/resty/v2"
+	"github.com/home-assistant/cli/client"
 	helper "github.com/home-assistant/cli/client"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -81,16 +80,7 @@ take Home Assistant backup on your system.`,
 		resp, err := request.Post(url)
 		ProgressSpinner.Stop()
 
-		// returns 200 OK or 400, everything else is wrong
-		if err == nil {
-			if resp.StatusCode() != 200 && resp.StatusCode() != 400 {
-				err = errors.New("Unexpected server response")
-				log.Error(err)
-			} else if !resty.IsJSONType(resp.Header().Get("Content-Type")) {
-				err = errors.New("API did not return a JSON response")
-				log.Error(err)
-			}
-		}
+		resp, err = client.GenericJSONErrorHandling(resp, err)
 
 		if err != nil {
 			fmt.Println(err)
