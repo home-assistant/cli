bd3f1657a999d0e939b2b5dae9159555b24b4d88
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2025-01-26 11:39:12 -0500

    add IP method "shared"

--- cmd/network_update.go
+++ cmd/network_update.go
@@ -75,7 +75,7 @@ func init() {
 
 	networkUpdateCmd.Flags().StringArray("ipv4-address", []string{}, "IPv4 address for the interface in the 192.168.1.5/24")
 	networkUpdateCmd.Flags().String("ipv4-gateway", "", "The IPv4 gateway the interface should use")
-	networkUpdateCmd.Flags().String("ipv4-method", "", "Method on IPv4: static|auto|disabled")
+	networkUpdateCmd.Flags().String("ipv4-method", "", "Method on IPv4: static|auto|disabled|shared")
 	networkUpdateCmd.Flags().StringArray("ipv4-nameserver", []string{}, "IPv4 address of upstream DNS servers. Use multiple times for multiple servers.")
 
 	networkUpdateCmd.Flags().StringArray("ipv6-address", []string{}, "IPv6 address for the interface in the 2001:0db8:85a3:0000:0000:8a2e:0370:7334/64")
