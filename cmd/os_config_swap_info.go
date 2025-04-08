b4329b776f69839c2f258ad200c46742b7136a44
Jan Čermák <sairon@users.noreply.github.com>
2025-04-08 12:42:28 +0200

    Add `ha os config swap` command set (#555)
    Add commands interfacing with the swap API added in
    home-assistant/supervisor#5770

---
+++ cmd/os_config_swap_info.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var osConfigSwapInfoCmd = &cobra.Command{
+	Use:     "info",
+	Aliases: []string{"in", "info"},
+	Short:   "Show HAOS swap settings",
+	Long: `
+This command allows you to see how swap is used by the Home Assistant OS.`,
+	Example: `
+  ha os config swap info`,
+	ValidArgsFunction: cobra.NoFileCompletions,
+	Args:              cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("os config swap info")
+
+		section := "os"
+		command := "config/swap"
+
+		resp, err := helper.GenericJSONGet(section, command)
+		if err != nil {
+			fmt.Println(err)
+			ExitWithError = true
+		} else {
+			ExitWithError = !helper.ShowJSONResponse(resp)
+		}
+	},
+}
+
+func init() {
+	osConfigSwapCmd.AddCommand(osConfigSwapInfoCmd)
+}
