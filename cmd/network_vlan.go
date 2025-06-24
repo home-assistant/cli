0b201388ce46e9ffb34ad0a7b54de50b0669477b
Jan Čermák <sairon@sairon.cz>
2025-06-24 11:02:52 +0200

    Clarify help text for ha network update/vlan addresses
    The help text for addresses in ha network commands was badly worded,
    probably missing the "format" word. State it takes the "CIDR notation"
    and use the former example in parenthesis.
    
    Fixes #571

--- cmd/network_vlan.go
+++ cmd/network_vlan.go
@@ -69,12 +69,12 @@ This function works only on an ethernet interface!
 
 func init() {
 
-	networkVlanCmd.Flags().StringArray("ipv4-address", []string{}, "IPv4 address for the interface in the 192.168.1.5/24")
+	networkVlanCmd.Flags().StringArray("ipv4-address", []string{}, "IPv4 address for the interface in CIDR notation (e.g. 192.168.1.5/24)")
 	networkVlanCmd.Flags().String("ipv4-gateway", "", "The IPv4 gateway the interface should use")
 	networkVlanCmd.Flags().String("ipv4-method", "", "Method on IPv4: static|auto|disabled")
 	networkVlanCmd.Flags().StringArray("ipv4-nameserver", []string{}, "IPv4 address of upstream DNS servers. Use multiple times for multiple servers.")
 
-	networkVlanCmd.Flags().StringArray("ipv6-address", []string{}, "IPv6 address for the interface in the 2001:0db8:85a3:0000:0000:8a2e:0370:7334/64")
+	networkVlanCmd.Flags().StringArray("ipv6-address", []string{}, "IPv6 address for the interface in CIDR notation (e.g. 2001:0db8:85a3:0000:0000:8a2e:0370:7334/64)")
 	networkVlanCmd.Flags().String("ipv6-gateway", "", "The IPv6 gateway the interface should use")
 	networkVlanCmd.Flags().String("ipv6-method", "", "Method on IPv6: static|auto|disabled")
 	networkVlanCmd.Flags().String("ipv6-addr-gen-mode", "", "IPv6 address generation mode: eui64|stable-privacy|default-or-eui64|default")
