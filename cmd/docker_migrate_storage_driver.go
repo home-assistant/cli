2c2df8f33851d6e85a9563fcd92de3a3e0b03417
Jan Čermák <sairon@users.noreply.github.com>
2025-11-27 17:36:33 +0100

    Add `docker migrate-storage-driver` command (#613)
    * Add `docker migrate-storage-driver` command
    
    Implement CLI for home-assistant/supervisor#6361. Without any extra
    argument, the command migrates to the overlayfs Containerd snapshotter,
    but the driver can be specified and will be validated by the Supervisor
    API - no need for another validation layer here. The list of driver is
    only used for shell completion.
    
    * Add more details to the confirmation prompt

---
+++ cmd/docker_migrate_storage_driver.go
@@ -0,0 +1,78 @@
+package cmd
+
+import (
+	helper "github.com/home-assistant/cli/client"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var dockerMigrateStorageCmd = &cobra.Command{
+	Use:   "migrate-storage-driver [driver]",
+	Short: "Migrate Docker storage driver",
+	Long: `
+This command schedules a Docker storage driver migration to be performed on the
+next reboot. By default, it migrates to the (Containerd snapshotter) overlayfs.
+
+The migration will be applied during the next system reboot. A reboot is required
+to complete the migration.
+`,
+	Example: `
+  ha docker migrate-storage-driver
+  ha docker migrate-storage-driver overlayfs
+`,
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		return []string{"overlayfs", "overlay2"}, cobra.ShellCompDirectiveNoFileComp
+	},
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		log.WithField("args", args).Debug("docker migrate-storage-driver")
+
+		section := "docker"
+		command := "migrate-storage-driver"
+
+		// Default to overlayfs if no argument provided
+		storageDriver := "overlayfs"
+		if len(args) > 0 {
+			storageDriver = args[0]
+		}
+
+		confirmed, err := helper.AskForConfirmation(`
+This will schedule a Docker storage driver migration to "`+storageDriver+`".
+Make sure to create a full Home Assistant backup before proceeding.
+
+Internet connectivity is required for re-download of all the container images
+and it is recommended to have at least 50% of free storage.
+
+Once confirmed, the migration will be applied on the next system reboot.
+Are you sure you want to proceed?`, 0)
+
+		if err != nil {
+			cmd.PrintErrln("Aborted:", err)
+			ExitWithError = true
+			return
+		}
+
+		if confirmed {
+			options := map[string]any{
+				"storage_driver": storageDriver,
+			}
+			resp, err := helper.GenericJSONPost(section, command, options)
+			if err != nil {
+				helper.PrintError(err)
+				ExitWithError = true
+			} else {
+				ExitWithError = !helper.ShowJSONResponse(resp)
+			}
+		} else {
+			cmd.PrintErrln("Aborted.")
+			ExitWithError = true
+		}
+	},
+}
+
+func init() {
+	dockerCmd.AddCommand(dockerMigrateStorageCmd)
+}
